82e97bda5b7b0b5becf9e0f063af9db6db614111
datkie <74259871+datkie@users.noreply.github.com>
2021-01-27 12:31:38 -0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var first = function(){
+    document.querySelector(".welcme").innerHTML = "Page Under Construction";
+    document.querySelector(".wrap").style.backgroundColor ="black";
+    document.querySelector(".wrap").style.color ="white";
+    document.querySelector(".des").innerHTML = "Thanks fo visiting my website this page is under construction "+ "<br>"+
+    "Please comeback later";
+    document.querySelector(".get").style.display ="none";
+}
